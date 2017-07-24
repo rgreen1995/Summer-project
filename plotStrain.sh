781fe260702e778a98bb3b1e52f46fc547b5315d
Rhys Green <c1572221@raven14.bullx>
2017-07-24 11:25:08 +0100

    removing the final/

--- plotStrain.sh
+++ plotStrain.sh
@@ -3,35 +3,35 @@
 #Specific run we're looking at
 RUN=$1
 
-python whitened.py --input-file final/${RUN}/output.hdf \
+python whitened.py --input-file ${RUN}/output.hdf \
                    --plot-map-waveforms \
                    --min-xlim -2.5 \
-                   --injection-file final/${RUN}/injection.xml.gz \
-                   --output-file final/${RUN}/strain_plot25
+                   --injection-file ${RUN}/injection.xml.gz \
+                   --output-file ${RUN}/strain_plot25
 
-python whitened.py --input-file final/${RUN}/output.hdf \
+python whitened.py --input-file ${RUN}/output.hdf \
                    --plot-map-waveforms \
                    --min-xlim -1.5 \
-                   --injection-file final/${RUN}/injection.xml.gz \
-                   --output-file final/${RUN}/strain_plot15
+                   --injection-file ${RUN}/injection.xml.gz \
+                   --output-file ${RUN}/strain_plot15
 
-python whitened.py --input-file final/${RUN}/output.hdf \
+python whitened.py --input-file ${RUN}/output.hdf \
                    --plot-map-waveforms \
                    --min-xlim -1.0 \
-                   --injection-file final/${RUN}/injection.xml.gz \
-                   --output-file final/${RUN}/strain_plot10
+                   --injection-file ${RUN}/injection.xml.gz \
+                   --output-file ${RUN}/strain_plot10
 
-python whitened.py --input-file final/${RUN}/output.hdf \
+python whitened.py --input-file ${RUN}/output.hdf \
                    --plot-map-waveforms \
                    --min-xlim -0.5 \
-                   --injection-file final/${RUN}/injection.xml.gz \
-                   --output-file final/${RUN}/strain_plot05
+                   --injection-file ${RUN}/injection.xml.gz \
+                   --output-file ${RUN}/strain_plot05
 
-python whitened.py --input-file final/${RUN}/output.hdf \
+python whitened.py --input-file ${RUN}/output.hdf \
                    --plot-map-waveforms \
                    --min-xlim -0.2 \
-                   --injection-file final/${RUN}/injection.xml.gz \
-                   --output-file final/${RUN}/strain_plot02
+                   --injection-file ${RUN}/injection.xml.gz \
+                   --output-file ${RUN}/strain_plot02
 
 # Rename dictionary
-mv final/${RUN}/strain_plot_dic.npy final/${RUN}/mapDic.npy
+#mv ${RUN}/strain_plot_dic.npy final/${RUN}/mapDic.npy
