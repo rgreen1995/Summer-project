889c14ce6e869a1f6402ea47d731eaaadfa9fce7
rgreen1995 <rgreen1995@gmail.com>
2017-08-04 17:21:26 +0100

    Update injected.py

--- injected.py
+++ injected.py
@@ -35,26 +35,25 @@ directorys=opts.output_file
 folder=directorys[:21]
 print folder
 
-## Load dictionary and make array of injected parameters
-#dic_name="d.npy"
-#dict_load=folder+dic_name
-#injected=numpy.load("%s" % dict_load).item()
-## Generate array manually
-inj_vals=list([1126259462.0,     # time
-              injected["mass1"], # mass1
-              injected["mass2"], # mass2
-              injected["spin1_a"], # spin 1 magnitude
-              0, # spin1 azimuthal
-              injected["spin1_polar"], # spin 1 polar
-              injected["spin2_a"], # spin 2 magnitude
-              0, # spin2 azimuthal
-              injected["spin2_polar"], # spin 2 polar
-              injected["distance"], # distance
-              1.5, # coa_phase]
-              injected["inclination"], # inclination
-              injected["polarization"], # polarisation
-              injected["ra"],
-              injected["dec"]])
+tc_inj= 1126259462.0 
+mass1_inj = 80. 
+mass2_inj = 10. 
+ra_inj = 2.21535724066 
+dec_inj = -1.23649695537 
+inclination_inj = 1.047197551 
+coa_phase_inj = 0.0 
+polarization_inj = 0.8 
+theta_JN_inj = 2.7 
+distance_inj = 500000
+
+spin1_a_inj = 0.75 
+spin1_azimuthal_inj = 0.0
+spin1_polar_inj =0.0
+spin2_a_inj = 0.0 
+spin2_azimuthal_inj = 0.0
+spin2_polar_inj =0.0
+
+injvals=[tc_inj,mass1_inj,mass2_inj,spin1_a_inj,spin1_azimuthal_inj,spin1_polar_inj,spin2_a_inj,spin2_azimuthal_inj, spin2_polar_inj, distance_inj, coa_phase_inj, inclination_inj, polarization_inj, ra_inj, dec_inj ]
 
 snr_list=[]
 
@@ -82,17 +81,16 @@ for ifo in ['H1', 'L1']:
     wh_stilde = FrequencySeries(stilde / asd, delta_f=stilde.delta_f,
                                  epoch=stilde.epoch)
     wh_strain = wh_stilde.to_timeseries()
-    
+
     print "generating injected waveforms"
-    print sargs
-    print varargs
+
     genclass = generator.select_waveform_generator(fp.static_args['approximant'])
     gen = generator.FDomainDetFrameGenerator(
         genclass,
         detectors=['H1', 'L1'], epoch=stilde.epoch,
         variable_args=varargs,
         **sargs)
-    fis = gen.generate(*map(float,inj_vals)[ifo]
+    fis= gen.generate(tc=injvals[0], mass1= injvals[1], mass2=injvals[2], spin1_a= injvals[3], spin1_azimuthal= injvals[4], spin1_polar=injvals[5], spin2_a=injvals[6],spin2_azimuthal= injvals[7], spin2_polar=injvals[8], distance=injvals[9], coa_phase=injvals[10], inclination=injvals[11],polarization=injvals[12], ra=injvals[13], dec=injvals[14])[ifo]
     if len(fis) < len(psd):
         fis.resize(len(psd))
     elif len(psd) < len(fis):
@@ -105,7 +103,7 @@ for ifo in ['H1', 'L1']:
     try:
         gps_time = sargs['tc']
     except KeyError:
-        gps_time = 1126259462.008837
+        gps_time = map_values['tc']
     xmin = float(opts.min_xlim)
     xmax = 0.05
 
@@ -128,8 +126,7 @@ for ifo in ['H1', 'L1']:
         fi /= asd
         ti = fi.to_timeseries()
         ax.plot(ti.sample_times.numpy()-gps_time, ti.data, 'b-', lw=2, zorder=2)
-    m, i = match(ti, ts, psd=psd, low_frequency_cutoff=gen.current_params['f_lower'])
-    print "Match between map and injected is %.2f" % m
+    m, i = match(ti, ts, psd=psd, low_frequency_cutoff=gen.current_params['f_lower']
     ax.set_xlim(xmin, xmax)
     ax.set_ylim(ylim)
     ax.text(xmin, ylim[0], 'Match=%.2f' % m, ha='left', va='bottom', fontsize=12)
@@ -137,40 +134,8 @@ for ifo in ['H1', 'L1']:
     if ii == 2:
         ax.set_xlabel('GPS time - {} (s)'.format(gps_time))
 
-    ## Find and save SNR
-    snr = matched_filter(ti, y, psd=psd, low_frequency_cutoff=20.0) ## Should be SNR between injected waveform and whitened data
-    #snr_map=matched_filter(ts, y, psd=psd, low_frequency_cutoff=20.0) ## SNR between MAP waveform and whitened data
+ 
 
-   # Remove regions corrupted by filter wraparound
-    snr = snr[len(snr) / 4: len(snr) * 3 / 4]
-    snr_map = snr[len(snr_map) / 4: len(snr_map) * 3 / 4]
-    snr_list.append(snr)
-    snr_list.append(snr_map)
-'''
-savename=opts.output_file
-print savename
-figname=savename[:-2]+"_SNR"
-## Plot and save SNRs
-print "saving SNR figures"
-jj=0
-pyplot.figure()
-for ifo in ['H1', 'L1']: ### Will need to manually extend this for Virgo
-    jj+=1
-   # map_snr=snr_list[jj-1]
-    inj_snr=snr_list[jj]
-    pyplot.subplot(2,1,jj)
-    #pyplot.plot(map_snr.sample_times,abs(map_snr))
-    pyplot.ylabel("SNR")
-pyplot.xlabel("Time")
-pyplot.savefig("%s.png" % figname)
-'''
-
-
-print "save dictionary"
-savename=opts.output_file
-print savename
-savename=savename[:-2]+"_dic"
-numpy.save("%s" %  savename, MAPDic)
 
 fp.close()
 fig.savefig(opts.output_file, dpi=200, bbox_inches='tight')
