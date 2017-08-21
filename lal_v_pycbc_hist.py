b84742c8b3fe97e2f9f0fc8fadd6e8d9a6b3acc5
rgreen1995 <rgreen1995@gmail.com>
2017-08-21 14:13:57 +0100

    Update lal_v_pycbc_hist.py

--- lal_v_pycbc_hist.py
+++ lal_v_pycbc_hist.py
@@ -21,7 +21,8 @@ data_name="output.hdf"
 ## Determine what parameters to plot
 whatdo=raw_input("What parameters do you want to plot? \n")
 
-param=mass1
+## List of parameters add in as required
+params=np.array(["mass1","mass2",])
 
 
 def getParameter(parameter):
@@ -36,3 +37,62 @@ def getParameter(parameter):
       temp=getattr(samples,parameter)
       parameter_values=np.append(parameter_values,temp[-1])
    return parameter_values
+
+def plotPosterior(parameter):
+    ## Ensure m1>m2 in posteriors
+   if parameter=="mass1":
+      mass1=getParameter("mass1")
+      mass2=getParameter("mass2")
+      parameter_values=np.zeros(len(mass1))
+      for aa in range(len(mass1)):
+         parameter_values[aa]=max(mass1[aa],mass2[aa])
+ 
+   ## Ensure m2>m1 in posteriors
+   elif parameter=="mass2":
+      mass1=getParameter("mass1")
+      mass2=getParameter("mass2")
+      parameter_values=np.zeros(len(mass1))
+      for aa in range(len(mass1)):
+         parameter_values[aa]=min(mass1[aa],mass2[aa])
+   else:
+      parameter_values=getParameter(parameter)
+      values=len(parameter_values)
+   savename=folder+parameter   
+   values=len(parameter_values)
+   ## Find confidence intervals
+   parameter_values=np.sort(parameter_values)
+   lower_90=parameter_values[250]
+   upper_90=parameter_values[4749]
+   mean_val=np.average(parameter_values)
+   
+   ## Plot and save
+   plt.figure()
+   plt.title("%d data points" % (values))
+   plt.hist(parameter_values,50, normed=True, alpha=0.9)
+   plt.axvline(x=lower_90,linewidth=2,linestyle='dashed',color='k')
+   plt.axvline(x=mean_val,linewidth=2, color='k')
+   plt.axvline(x=upper_90,linewidth=2,linestyle='dashed',color='k')
+   plt.xlabel("%s" % parameter)
+   plt.grid()
+   # Removed the priors, add them if you fancy
+   ## Plot priors for derived spin parameters
+   #if parameter=="chi_p":
+      #prior=np.loadtxt("priors/chi_p_prior.txt")
+      #plt.hist(prior,50,normed=True,alpha=0.6)
+   #elif parameter=="chi_eff":
+      #prior=np.loadtxt("priors/chi_eff_prior.txt")
+      #plt.hist(prior,50,normed=True,alpha=0.6)
+   plt.savefig("%s.png" % savename)
+   print "Plot saved as %s.png" % savename
+
+
+## Execute
+if whatdo=="all":
+  print "Generating posteriors for all parameters..."
+  for aa in range(len(params)):
+    plotPosterior(params[aa])
+else:
+  print "Generating posterior for %s" % whatdo
+  plotPosterior(whatdo)
+
+print "DONE"
