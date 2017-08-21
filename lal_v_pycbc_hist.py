3d7a3558decc18e2c86090195322cbf0eb13e5c6
rgreen1995 <rgreen1995@gmail.com>
2017-08-21 14:07:07 +0100

    Update lal_v_pycbc_hist.py

--- lal_v_pycbc_hist.py
+++ lal_v_pycbc_hist.py
@@ -21,10 +21,9 @@ data_name="output.hdf"
 ## Determine what parameters to plot
 whatdo=raw_input("What parameters do you want to plot? \n")
 
-## List of parameters
-params=np.array(["mass1"])
+param=mass1
 
-## Function to extract posterior for a given parameter
+
 def getParameter(parameter):
    ## Prepare to read in parameters
    datafile=folder+data_name
@@ -37,48 +36,3 @@ def getParameter(parameter):
       temp=getattr(samples,parameter)
       parameter_values=np.append(parameter_values,temp[-1])
    return parameter_values
-## Extract parameter and plot posterior
-def plotPosterior(parameter):
-    parameter_values=getParameter(parameter)
-    values=len(parameter_values)
-
-savename=folder+parameter   
-values=len(parameter_values)
-   ## Find confidence intervals
-#   parameter_values=np.sort(parameter_values)
-#   lower_90=parameter_values[250]
-#  upper_90=parameter_values[4749]
-#  mean_val=np.average(parameter_values)
-   
-   ## Plot and save
-plt.figure()
-plt.title("%d data points" % (values))
-plt.hist(parameter_values,50, normed=True, alpha=0.9)
-plt.axvline(x=lower_90,linewidth=2,linestyle='dashed',color='k')
-plt.axvline(x=mean_val,linewidth=2, color='k')
-plt.axvline(x=upper_90,linewidth=2,linestyle='dashed',color='k')
-plt.xlabel("%s" % parameter)
-plt.grid()
-   # Removed the priors, add them if you fancy
-   ## Plot priors for derived spin parameters
-   #if parameter=="chi_p":
-      #prior=np.loadtxt("priors/chi_p_prior.txt")
-      #plt.hist(prior,50,normed=True,alpha=0.6)
-   #elif parameter=="chi_eff":
-      #prior=np.loadtxt("priors/chi_eff_prior.txt")
-      #plt.hist(prior,50,normed=True,alpha=0.6)
-plt.savefig("%s.png" % savename)
-print "Plot saved as %s.png" % savename
-
-
-## Execute
-if whatdo=="all":
-  print "Generating posteriors for all parameters..."
-  for aa in range(len(params)):
-    plotPosterior(params[aa])
-else:
-  print "Generating posterior for %s" % whatdo
-  plotPosterior(whatdo)
-
-print "DONE"
-
