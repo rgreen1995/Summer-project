da9639f3c141396f64bc77168b5b2ba54c0eb2b0
Rhys Green <c1572221@raven14.bullx>
2017-08-21 13:53:55 +0100

    changing indent errors

--- lal_v_pycbc_hist.py
+++ lal_v_pycbc_hist.py
@@ -40,10 +40,10 @@ def getParameter(parameter):
 ## Extract parameter and plot posterior
 def plotPosterior(parameter):
     parameter_values=getParameter(parameter)
-      values=len(parameter_values)
+    values=len(parameter_values)
 
-   savename=folder+parameter   
-   values=len(parameter_values)
+savename=folder+parameter   
+values=len(parameter_values)
    ## Find confidence intervals
 #   parameter_values=np.sort(parameter_values)
 #   lower_90=parameter_values[250]
@@ -51,14 +51,14 @@ def plotPosterior(parameter):
 #  mean_val=np.average(parameter_values)
    
    ## Plot and save
-   plt.figure()
-   plt.title("%d data points" % (values))
-   plt.hist(parameter_values,50, normed=True, alpha=0.9)
-   plt.axvline(x=lower_90,linewidth=2,linestyle='dashed',color='k')
-   plt.axvline(x=mean_val,linewidth=2, color='k')
-   plt.axvline(x=upper_90,linewidth=2,linestyle='dashed',color='k')
-   plt.xlabel("%s" % parameter)
-   plt.grid()
+plt.figure()
+plt.title("%d data points" % (values))
+plt.hist(parameter_values,50, normed=True, alpha=0.9)
+plt.axvline(x=lower_90,linewidth=2,linestyle='dashed',color='k')
+plt.axvline(x=mean_val,linewidth=2, color='k')
+plt.axvline(x=upper_90,linewidth=2,linestyle='dashed',color='k')
+plt.xlabel("%s" % parameter)
+plt.grid()
    # Removed the priors, add them if you fancy
    ## Plot priors for derived spin parameters
    #if parameter=="chi_p":
@@ -67,8 +67,8 @@ def plotPosterior(parameter):
    #elif parameter=="chi_eff":
       #prior=np.loadtxt("priors/chi_eff_prior.txt")
       #plt.hist(prior,50,normed=True,alpha=0.6)
-   plt.savefig("%s.png" % savename)
-   print "Plot saved as %s.png" % savename
+plt.savefig("%s.png" % savename)
+print "Plot saved as %s.png" % savename
 
 
 ## Execute
